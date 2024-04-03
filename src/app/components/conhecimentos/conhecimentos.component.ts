7cdc48244532a33c6c8e317701cd8544cabe5d01
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-04-03 08:38:11 -0300

    Adicionando responsividade ao componente de projetos e utilizando prettier como formatador de código

--- src/app/components/conhecimentos/conhecimentos.component.ts
+++ src/app/components/conhecimentos/conhecimentos.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-conhecimentos',
   templateUrl: './conhecimentos.component.html',
-  styleUrls: ['./conhecimentos.component.css']
+  styleUrls: ['./conhecimentos.component.css'],
 })
-export class ConhecimentosComponent {
-
-}
+export class ConhecimentosComponent {}
--- src/app/components/projetos/projetos.component.ts
+++ src/app/components/projetos/projetos.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projetos',
   templateUrl: './projetos.component.html',
-  styleUrls: ['./projetos.component.css']
+  styleUrls: ['./projetos.component.css'],
 })
-export class ProjetosComponent {
-
-}
+export class ProjetosComponent {}
