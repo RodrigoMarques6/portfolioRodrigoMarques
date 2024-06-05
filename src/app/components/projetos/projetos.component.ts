3c0f51a5186f8e5d2cd26603c6a076f9f7c7870f
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-06-04 22:40:05 -0300

    Lembrete adicionado ao componente projetos para ajustes gerais

--- src/app/components/projetos/projetos.component.ts
+++ src/app/components/projetos/projetos.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./projetos.component.css'],
 })
 export class ProjetosComponent {
-  projeto4 = 'Consumo de API - Tabela Campeonato Brasileiro Série B'
+  projeto1 = 'Landing Page Responsiva - Test';
+  projeto2 = 'Gerenciamento de Funcionários - CRUD Test';
+  projeto3 = 'App Arquitetura em Angular';
+  projeto4 = 'Consumo de API - Tabela Campeonato Brasileiro Série B';
 }
