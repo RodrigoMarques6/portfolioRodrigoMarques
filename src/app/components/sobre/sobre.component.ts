b4fd9338ac3162685caf01cae3318b8b26f324ce
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-09-13 22:09:16 -0300

    feat/instalação do tailwind e configuração

--- src/app/components/sobre/sobre.component.ts
+++ src/app/components/sobre/sobre.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class SobreComponent {
   sobreMim1: string =
-    'Meu nome é Rodrigo Marques de Lima, tenho 27 anos de idade e moro em Maceió-AL. Sou estudante de análise e desenvolvimento de sistemas e estou em busca da minha primeira oportunidade na área de programação, com foco em linguagens utilizadas na web.';
+    'Meu nome é Rodrigo Marques de Lima, tenho 28 anos de idade e moro em Maceió-AL. Sou estudante de análise e desenvolvimento de sistemas e estou em busca da minha primeira oportunidade na área de programação, com foco em linguagens utilizadas na web.';
   sobreMim2: string =
     'Sempre estou buscando estudar e desenvolver projetos web, em maior parte no Front-end, visando ampliar meus conhecimentos na área, bem como ganhar expertise para os desafios propostos.';
   curriculo: string = 'Visualizar currículo';
