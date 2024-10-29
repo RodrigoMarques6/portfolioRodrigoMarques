9fd3f2d9f10a937ec1a01775ec41b041884c1889
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-10-28 22:27:53 -0300

    feat/adição de botões ao sobremim

--- src/app/components/main/sobre/sobre.component.ts
+++ src/app/components/main/sobre/sobre.component.ts
@@ -8,9 +8,5 @@ import { Component } from '@angular/core';
 export class SobreComponent {
   sobreMim1: string =
     'Sou desenvolvedor front-end com dois anos de experiência em programação web. Transformo ideias em experiências digitais envolventes, criando interfaces que conectam pessoas e tecnologia de forma intuitiva. Que tal me conhecer melhor navegando por este portfólio? Fique à vontade!';
-  // Sou apaixonado por desenvolver experiências de usuário intuitivas e envolventes. Formado em Análise e Desenvolvimento de Sistemas,  busco constantemente ampliar conhecimentos e criar soluções eficientes. Estou pronto para contribuir com uma visão diferenciada para o desenvolvimento de soluções web criativas.';
-  // sobreMim2: string =
-  //   'Atuo principalmente no desenvolvimento Front-End, sempre buscando ampliar meus conhecimentos e ganhar expertise para enfrentar novos desafios. Minhas formações em Administração e Ciências Contábeis complementam minhas habilidades, proporcionando uma visão diferenciada para o desenvolvimento de soluções. Sou apaixonado por criar experiências de usuário cativantes, autodidata e sempre pronto para contribuir com soluções criativas';
   curriculo: string = 'Visualizar currículo';
-  sobre: string = 'Mais sobre mim (vai ter uma animação direta)'
 }
