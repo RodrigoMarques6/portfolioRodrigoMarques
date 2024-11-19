b6edcade21e2c1ec4d2326fb5e58b1231f2e1120
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2024-11-18 22:45:25 -0300

    feat/ reformulando área de experiÊncias do projeto

--- src/app/components/main/experiencias/experiencias.component.ts
+++ src/app/components/main/experiencias/experiencias.component.ts
@@ -8,28 +8,30 @@ import { Component } from '@angular/core';
 export class ExperienciasComponent {
   descricaoJuceal: string[] = [
     'Suporte técnico em atendimento ao Portal Facilita AL;',
+    'Atendimento telefônico para dúvidas a respeito do portal;',
     'Atendimento presencial;',
     'Análise de aberturas, baixas e alterações de empresas de qualquer porte.',
   ];
   descricaoSenai: string[] = [
     'Desenvolvimento Web Front-End em projetos complexos;',
     'Reprodução de telas Front-End com base no Figma;',
-    'Metodologia Scrum: daily, planning, retrospectiva e review;',
+    'Metodologia Scrum: daily/planning/retrospective/review;',
     'Azure Devops para organização de tasks;',
     'Controle de versão em atuação com grandes times - GIT;',
     'Suporte Técnico em sistemas web por email.',
   ];
 
   descricaoYellowKite: string[] = [
-    'Treinamento para usuários de plataformas conversacionais;',
+    'Desenvolvimento de landing pages responsivas;',
+    'Manutenção de websites do portfólio da empresa;',
     'Integrações com APIs do RD Conversas, RD Marketing, RD CRM, Hablla e Bitrix24 para desenvolvimento de chatbots;',
     'Automações com uso de IA para tarefas rotineiras dos setores de marketing e clientes da agência;',
-    'Desenvolvimento de landing pages responsivas;',
-    'Manutenção de sites já existentes no portfólio da empresa;',
+    'Treinamento em plataformas conversacionais.',
   ];
   descricaoSoftbase: string[] = [
     'Desenvolvimento Front e Back;',
     'Reprodução de telas Front-End com base no Figma;',
+    'Foco na experiência do usuário - UX;',
     'Metodologia Scrum;',
     'Controle de versão - GIT;',
     'Click-up como controle de tasks;',
